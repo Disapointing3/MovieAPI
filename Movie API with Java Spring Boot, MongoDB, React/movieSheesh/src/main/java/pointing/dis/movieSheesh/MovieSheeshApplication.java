6ed9c8868c7451092f571dcb7ef352f536a4dc8c
Timothy Paek <tax2farm@gmail.com>
2023-04-16 21:09:28 -0400

    basic setup stuff

---
+++ Movie API with Java Spring Boot, MongoDB, React/movieSheesh/src/main/java/pointing/dis/movieSheesh/MovieSheeshApplication.java
@@ -0,0 +1,16 @@
+package pointing.dis.movieSheesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MovieSheeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSheeshApplication.class, args);
+	}
+
+
+
+}
---
+++ Movie API with Java Spring Boot, MongoDB, React/movieSheesh/src/test/java/pointing/dis/movieSheesh/MovieSheeshApplicationTests.java
@@ -0,0 +1,13 @@
+package pointing.dis.movieSheesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSheeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
